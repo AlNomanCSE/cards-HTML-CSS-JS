6bebe090b8b3bfc9761c23f6ea82de9796d50802
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-01-05 12:49:46 +0600

    sign up card added

---
+++ Sign Up Form/index.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+const siginupBtn = document.querySelector(".green-bg button");
+siginupBtn.addEventListener("click", () => {
+  container.classList.toggle("change");
+});
