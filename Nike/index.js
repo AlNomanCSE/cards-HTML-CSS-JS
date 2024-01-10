1d055b3f0b58982614250415ac4057136dd1b80d
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-01-10 15:10:00 +0600

    added a card

---
+++ Nike/index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+const color_1 = document.querySelector(".color-1");
+const color_2 = document.querySelector(".color-2");
+
+color_2.addEventListener("click", () => {
+  container.classList.add("change");
+});
+color_1.addEventListener("click", () => {
+  container.classList.toggle("change");
+});
