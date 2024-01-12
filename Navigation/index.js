d67386ff7a9faaea202018532b95de227bedf713
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-01-12 17:00:58 +0600

    circular navigation added

---
+++ Navigation/index.js
@@ -0,0 +1,7 @@
+const dots_btn = document.querySelector(".dots_btn");
+
+dots_btn.addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+  console.log("ok");
+});
+    
